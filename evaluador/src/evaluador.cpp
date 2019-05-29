220eac476f53725e00d81ab1f7b5611c6436e4a7
jalzat39 <jmiguelalzate@gmail.com>
2019-05-29 14:34:47 -0500

    creacion de branch

--- evaluador/src/evaluador.cpp
+++ evaluador/src/evaluador.cpp
@@ -1,1 +1,19 @@
-#include<>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <semaphore.h>
+#include "elementos.h"
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+void main(int argc, char *argv[]){
+    int registro [5][2];
+
+
+}
